ac2debc86f34cde00acf47740b98bb68b13afb1a
Jorge Matos <jisapjisap@gmail.com>
2026-05-03 11:07:40 +0200

    feat: add global CSS configuration and WhyChooseUs feature component

--- src/features/home/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/features/home/components/WhyChooseUs/WhyChooseUs.jsx
@@ -3,14 +3,15 @@ import appointment from "./assets/appointment.png"
 import WhyChooseUsImg from "./assets/video-bg-image.jpg"
 import { Play } from "lucide-react"
 import { Link } from "react-router-dom"
+import Image from "../../../../shared/ui/Image"
 
 
 const WhyChooseUs = () => {
   return (
     <>
       <div className="bg-heading">
-        <section className="why-choose-us section section-container py-[8%] relative space-y-10">
-          <div className="content-col text-white w-full lg:w-1/2 relative gap-0" data-aos="fade-right">
+        <section className="why-choose-us section section-container py-[8%] relative flex flex-col lg:flex-row items-center gap-10 lg:gap-20">
+          <div className="content-col text-white w-full lg:w-1/2 relative" data-aos="fade-right">
             <div className="main-title flex flex-col gap-3">
               <span className="sub-title" data-aos="fade-right" data-aos-delay="100">
                 Why Choose Us
@@ -66,11 +67,23 @@ const WhyChooseUs = () => {
                       </p>
                     </div>
                   </li>
-
                 </ul>
               </div>
+            </div>
+          </div>
 
+          <div className="relative w-full lg:w-1/2 h-full lg:h-150" data-aos="fade-left" data-aos-delay="400">
+            <Image
+              src={WhyChooseUsImg}
+              alt="Why Choose Us"
+              wrapperClass="h-full"
+              className="rounded-2xl"
+            />
 
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="play-btn relative w-20 h-20 lg:w-25 lg:h-25 bg-white rounded-full flex items-center justify-center shadow-lg cursor-pointer">
+                <Play size={35} />
+              </div>
             </div>
           </div>
         </section>
