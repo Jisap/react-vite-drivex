f8798c4a8b77237eaa6f68135e783d2ade1e506d
Jorge Matos <jisapjisap@gmail.com>
2026-05-05 15:53:46 +0200

    feat: create reusable CommentForm component for user feedback

--- src/shared/ui/CommentForm.jsx
+++ src/shared/ui/CommentForm.jsx
@@ -19,6 +19,41 @@ const CommentForm = () => {
               className='w-full bg-gray-50 border border-gray-200 resize-none rounded-lg p-5 focus:outline-none focus:border-primary transition'
             ></textarea>
           </div>
+
+          <div className='grid gird-cols-1 md:grid-cols-3 gap-6'>
+            <div data-aos="fade-center" data-aos-delay="900">
+              <label className='block mb-2 text-lg'>
+                Name <span className='text-red-500'>*</span>
+              </label>
+
+              <input
+                type="text"
+                className='w-full bg-gray-50 border border-gray-200 rounded-lg p-3 focus:outline-none focu:border-primary transition'
+              />
+            </div>
+
+            <div data-aos="fade-center" data-aos-delay="1000">
+              <label className='block mb-2 text-lg'>
+                Email <span className='text-red-500'>*</span>
+              </label>
+
+              <input
+                type="email"
+                className='w-full bg-gray-50 border border-gray-200 rounded-lg p-3 focus:outline-none focu:border-primary transition'
+              />
+            </div>
+
+            <div data-aos="fade-center" data-aos-delay="1100">
+              <label className='block mb-2 text-lg'>
+                Website <span className='text-red-500'>*</span>
+              </label>
+
+              <input
+                type="text"
+                className='w-full bg-gray-50 border border-gray-200 rounded-lg p-3 focus:outline-none focu:border-primary transition'
+              />
+            </div>
+          </div>
         </form>
       </div>
     </>
