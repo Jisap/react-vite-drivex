91b52ffb1459c4ccef0b09ddf2bb2c330fe67677
Jorge Matos <jisapjisap@gmail.com>
2026-05-06 15:44:15 +0200

    feat: create Service page to display service offerings with card grid and banners

--- src/features/home/pages/Service.jsx
+++ src/features/home/pages/Service.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import PageBanner from '../../../shared/ui/PageBanner'
+import ServiceCard from '../../../shared/ui/ServiceCard'
+import Testimonials from '../../../shared/components/Testimonials/Testimonials'
+import Partners from '../../../shared/components/Partners/Partners'
+import { services } from '../data/services'
+
+
 
 
 const Service = () => {
@@ -9,6 +15,25 @@ const Service = () => {
         title="Our Services"
         currentPage="Services"
       />
+
+      <div className="section py-[8%] relative">
+        <div className='grid-col lg:grid-cols-4 gap-10'>
+          {services.map((service, i) => (
+            <div
+              key={service.id}
+              data-aos="fade-up"
+              data-aos-delay={i * 120}
+            >
+              <ServiceCard
+                id={service.id}
+                image={service.image}
+                icon={service.icon}
+                title={service.title}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
