5233e20ba580d792326c40c1dcfbaaab0d6cb25a
Jorge Matos <jisapjisap@gmail.com>
2026-05-06 18:36:03 +0200

    feat: add Cars page and reusable CarList component

--- src/features/home/pages/Cars.jsx
+++ src/features/home/pages/Cars.jsx
@@ -1,13 +1,25 @@
-import PageBanner from "../../../shared/ui/PageBanner"
-
+import PageBanner from "../../../shared/ui/PageBanner";
+import { cars } from "../../../shared/data/cars";
+import CarList from "../../../shared/ui/CarList";
 
 const Cars = () => {
+
+  const { currentCars } = useCarFilter(cars)
+
   return (
     <>
       <PageBanner
         title="Our Cars"
         currentPage="Cars"
       />
+
+      <div className="section section-container items-start! py-[8%] flex flex-col lg:flex-row gap-10">
+        <div className="w-full lg:w-[70%]">
+          <CarList
+            currentCars={currentCars}
+          />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/shared/ui/CarList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import CarCard from './CarCard'
+
+const CarList = ({ cars }) => {
+  return (
+    <>
+      <div className='grid lg:grid-cols-2 xl:grid-cols-3 gap-10'>
+        {cars.length > 0 ? (
+          cars.map((car, index) => (
+            <div
+              key={car.id}
+              data-aos="fade-up"
+              data-aos-delay={index * 100}
+            >
+              <CarCard car={car} />
+            </div>
+          ))
+        ) : (
+          <p>
+            No Car Found
+          </p>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default CarList
