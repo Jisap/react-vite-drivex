c8b840d16835e3c6dd0f10b1a546e021d6fc76e2
Jorge Matos <jisapjisap@gmail.com>
2026-05-06 17:02:10 +0200

    feat: create Testimonials page component with responsive grid layout

--- src/features/home/pages/Testimonials.jsx
+++ src/features/home/pages/Testimonials.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import TestimonialsCard from '../../../shared/ui/TestimonialsCard'
+import PageBanner from '../../../shared/ui/PageBanner'
+import { testimonials } from '../../../shared/data/testimonials'
 
 const Testimonials = () => {
   return (
@@ -8,7 +11,24 @@ const Testimonials = () => {
         currentPage="Testimonials"
       />
 
-
+      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 section py-[8%] relative'>
+        {testimonials.map((tst, index) => (
+          <div
+            key={tst.id}
+            className='py-5'
+            data-aos="fade-up"
+            data-aos-delay={150 * index}
+          >
+            <TestimonialsCard
+              id={tst.id}
+              image={tst.image}
+              name={tst.name}
+              position={tst.position}
+              pera={tst.pera}
+            />
+          </div>
+        ))}
+      </div>
     </>
   )
 }
